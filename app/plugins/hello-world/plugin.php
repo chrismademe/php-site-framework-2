2869f64649bb7bcaeb4ae2770f0565481b886e7e
Chris Galbraith <chris@chrismade.me>
2015-11-01 17:06:00 +0000

    Add function exists check

--- app/plugins/hello-world/plugin.php
+++ app/plugins/hello-world/plugin.php
@@ -8,14 +8,18 @@
  * manipulate site data.
  */
 
-function hello_world() {
-    global $variables; // Get Variables object
+if ( !function_exists('hello_world') ) {
 
-    // Run only on the homepage
-    if ( $variables->page['slug'] == 'homepage' ) {
-        $variables->addVar('hello_world', 'Hello World!'); // Add a new theme variable
+    function hello_world() {
+        global $variables; // Get Variables object
+
+        // Run only on the homepage
+        if ( $variables->page['slug'] == 'homepage' ) {
+            $variables->addVar('hello_world', 'Hello World!'); // Add a new theme variable
+        }
     }
-}
 
-// Add the action to on_model_init's trigger
-$triggers->addAction('on_model_init', 'hello_world'); // Run plugin on model load.
+    // Add the action to on_model_init's trigger
+    $triggers->addAction('on_model_init', 'hello_world'); // Run plugin on model load.
+
+}
