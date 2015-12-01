f880949c64278dd17e96db69ab28657d11690de7
Chris Galbraith <chris.galbraith@searchitlocal.co.uk>
2015-12-01 16:28:13 +0000

    Fix meta plugin

--- app/plugins/meta/plugin.php
+++ app/plugins/meta/plugin.php
@@ -22,12 +22,12 @@ if ( !file_exists(META_FILE) || !is_readable(META_FILE) ) {
  */
 $meta_data = json_decode( file_get_contents(META_FILE), TRUE );
 
-if ( $meta_data && array_key_exists($variables->page['slug'], $meta_data) ) {
+if ( $meta_data && array_key_exists($variables->get('page|slug'), $meta_data) ) {
 
     // Merge data
-    $data = array_merge($variables->page['meta'], $meta_data[$variables->page['slug']]);
+    $data = array_merge($variables->get('page|meta'), $meta_data[$variables->get('page|slug')]);
 
     // Set meta
-    $variables->page['meta'] = $data;
+    $variables->extend('page', 'meta', $data);
 
 }
